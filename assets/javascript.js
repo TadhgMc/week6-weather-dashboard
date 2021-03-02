dc6f8d30f7c6736fd9b92a06a19ca7ebf2c3c606
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-02 11:01:22 -0500

    adding variables to javascript to indicate to html elements

---
+++ assets/javascript.js
@@ -0,0 +1,8 @@
+var searchForm = $('.seachForm'); //whole form element with the search bar
+var searchBox = $('#searchBox'); // specific input box for searching cities
+var currentDay = $('.currentDay'); //whole card for surrent day
+var firstDay = $('.1day'); //whole card for first day
+var secondDay = $('.2day'); //whole card for second day
+var thirdDay = $('.3day'); //whole card for third day
+var fourthDay = $('.4day'); //whole card for fourth day
+var fifthDay = $('.5day'); //whole card for fifth day
