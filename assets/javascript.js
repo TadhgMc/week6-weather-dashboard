d3a44ac54545c3d7c93eee9d7f35ca31e7ac40ce
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-03 17:06:08 -0500

    properly structured function to work, and created console logs that show that page is correctly taking the search input, and adding it to the api url, and receiving data from the api, and one series needed to get needed weather data

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,7 @@
 var searchForm = $('.seachForm'); //whole form element with the search bar
-var searchBox = $('#searchBox'); // specific input box for searching cities
-var currentDay = $('.currentDay'); //whole card for surrent day
+var searchInput = $('.searchInput'); // specific input box for searching cities
+var searchBtn = $('.searchBtn'); //class calling to search button
+var currentDay = $('.currentDay'); //whole card for current day
 var firstDay = $('.1day'); //whole card for first day
 var secondDay = $('.2day'); //whole card for second day
 var thirdDay = $('.3day'); //whole card for third day
@@ -22,26 +23,35 @@ weather icon <weather.icon>, temp <main.temp>, humidity <main.humidity>, wind sp
 // API DOCS forecasted weather https://openweathermap.org/forecast5
 // example url http://api.openweathermap.org/data/2.5/forecast?q=columbus&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a
 /* will always need date <list.dt>, icon of weather conditions <list.weather.icon>, temp <list.main.temp>, humidity <list.main.humidity> */
+var weatherUrl = "";
+searchBtn.click(function (event) {
+    event.preventDefault();
+    getWeather();
+}); 
 
+console.log(searchInput.val());
 
-var checkHere = searchBox.text;
-var weatherUrl = `http://api.openweathermap.org/data/2.5/find?q=${checkHere}&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a`
-console.log(checkHere);
+function getWeather (){
+    //make a check to see if something has been searched before, and get repeat search info from local storage --have the buttons point to their own localstorage
+    
+    var checkHere = searchInput.val();
+    weatherUrl = "http://api.openweathermap.org/data/2.5/find?q="+ checkHere +"&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a"
+    console.log(checkHere);
+    console.log(weatherUrl);
+    
+    
 
-
-function getWeather (sentUrl){
-    //make a check to see if something has been searched before, and get repeat search info from local storage
-
-
-    fetch(sentUrl)
-    .then(function(response) {
+    fetch(weatherUrl)
+    .then(function (response) {
+        //make if ; if bad response, say so
         console.log(response)
-        //return response.json();
+        return response.json();
     })
-    .then(function(data) {
-
+    .then( function (data) {
+        console.log(data);
+        console.log(data.list[0].main.temp)
     })
 }
 
 
-searchForm.on("submit", getWeather(weatherUrl)); 
+
